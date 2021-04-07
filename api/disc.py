e9780068f44eae975f9d4916e3344b4593ce8f18
Edward Lim <Edward.Lim@redcypress.com.tw>
2021-04-07 12:05:08 +0800

    integrate fe and be

--- api/disc.py
+++ api/disc.py
@@ -1,30 +1,23 @@
-from flask import Flask, jsonify, redirect, request, url_for
-from pymongo import MongoClient
-from flask_cors import CORS, cross_origin
+from flask import Blueprint, jsonify, redirect, request, url_for
 import json
+from pymongo import MongoClient
 
-app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
-
+disc = Blueprint('disc', __name__)
 
 mongoClient = MongoClient('mongodb://127.0.0.1:27017')
 db = mongoClient.get_database('disc_db')
 disc_col = db.get_collection('disc_col')
 
-@app.route('/addRecord', methods=['POST'])
+@disc.route('/addRecord', methods=['POST'])
 def addRecord():
     data = json.loads(request.data.decode('utf-8'))
     disc_col.insert_one({"name": data['name'], "position": data['position'], "results": data['results']})
     return jsonify({})
 
-@app.route('/getRecords')
+@disc.route('/getRecords')
 def getRecords():
     records_json = []
     if disc_col.find({}):
         for record in disc_col.find({}).sort('_id', -1):
             records_json.append({"name": record['name'], "id": str(record['_id']), "position": record['position'], "results": record['results'], "time": str( record['_id'].generation_time )})
     return json.dumps(records_json)
-
-if __name__ == "__main__":
-    app.run(debug=True)
